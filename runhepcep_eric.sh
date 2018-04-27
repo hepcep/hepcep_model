3cba976c0ecc0de5f11fa7a099cf45f92f8ad9dc
Eric Tatara <tatara@anl.gov>
2018-04-27 17:31:51 -0500

    emews project components

--- runhepcep_eric.sh
+++ runhepcep_eric.sh
@@ -2,6 +2,6 @@
 #
 # Run the chi-sim for Eric's system
 
-export LD_LIBRARY_PATH=/mnt/e/repast_hpc-2.2.0/ext/lib:/mnt/e/ANL/Projects/HepCEP/chiSIM-0.2/lib
-mpirun -n 1 ./Debug/hepcep_model-0.0 ./config/model.props
-
+#export LD_LIBRARY_PATH=/mnt/e/repast_hpc-2.2.0/ext/lib:/mnt/e/ANL/Projects/HepCEP/chiSIM-0.2/lib
+#mpirun -n 1 
+./Debug/hepcep_model-0.0 ./config/model.props ./config/config.props 

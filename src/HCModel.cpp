3f49dbbb900564fdc3d3fc70f93693a02342f11f
Eric <tatara@anl.gov>
2019-11-03 12:10:13 -0600

    Use start time + 1 for checkpoint runs

--- src/HCModel.cpp
+++ src/HCModel.cpp
@@ -189,7 +189,12 @@ HCModel::HCModel(repast::Properties& props, unsigned int moved_data_size) :
 			}
 			local_persons.emplace(id, (*iter));
 		}
-		start_at = floor(serialized_at) + .000000000001;
+        // The start time for regular actions is at the next integer from the serialization time
+		start_at = floor(serialized_at) + 1; //.000000000001;
+        
+        // Perform an initial zone census
+        zoneCensus();
+        
 		personCreator = std::make_shared<PersonCreator>(max_id + 1);
 		std::cout << "Resuming from " << fname << ", starting at: " << std::setprecision(20) << std::fixed << start_at << std::endl;
 	} else {

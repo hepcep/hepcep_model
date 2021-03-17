f2b2781ebba8dfbe6d2280c91485dffc5c11df4e
Nick Collier <ncollier@anl.gov>
2021-03-17 11:16:16 -0500

    Updated sweep submission script

--- swift_proj/swift/bebop_run_hepcep_sweep.sh
+++ swift_proj/swift/bebop_run_hepcep_sweep.sh
@@ -21,12 +21,8 @@ export TURBINE_OUTPUT=$EMEWS_PROJECT_ROOT/experiments/$EXPID
 check_directory_exists
 
 # TODO edit the number of processes as required.
-<<<<<<< HEAD
 # 288
-export PROCS=252
-=======
 export PROCS=72
->>>>>>> 08f196f6d93ea424d334e919d96b62194e0a5a48
 
 # TODO edit QUEUE, WALLTIME, PPN, AND TURNBINE_JOBNAME
 # as required. Note that QUEUE, WALLTIME, PPN, AND TURNBINE_JOBNAME will
@@ -100,10 +96,6 @@ set -x
 
 swift-t -n $PROCS $MACHINE -p -r $MODEL_DIR -I $MODEL_DIR \
   $EMEWS_PROJECT_ROOT/swift/hepcep_sweep.swift \
-<<<<<<< HEAD
   -f="$EMEWS_PROJECT_ROOT/data/upf_moud_m_only.txt" \
-=======
-  -f="$EMEWS_PROJECT_ROOT/data/upf_enrollment_sweep_retreat_lim_adherence.txt" \
->>>>>>> 08f196f6d93ea424d334e919d96b62194e0a5a48
   -config_file=$CONFIG_FILE \
   $CMD_LINE_ARGS
